2d98add698c89a8ba8282a19cd5872fa082ce9a1
nguyen hao <nguyenhao194554@gmail.com>
2022-05-11 00:05:41 +0700

    add background login

--- src/javaswingdev/main/Main.java
+++ src/javaswingdev/main/Main.java
@@ -2,7 +2,7 @@
 package javaswingdev.main;
 
 import java.awt.Color;
-
+import javax.swing.Timer;
 
 public class Main extends javax.swing.JFrame {
 
@@ -10,6 +10,8 @@ public class Main extends javax.swing.JFrame {
     /**
      * Creates new form Login
      */
+    Timer timer;
+
     public Main() {
         initComponents();
         init();
@@ -18,7 +20,7 @@ public class Main extends javax.swing.JFrame {
     private void init() {
         main = this;
         titleBar.initJFram(this);
-        alertLabel.setVisible(false);
+             alertLabel.setVisible(false);
         setLocationRelativeTo(null);
     }
     
@@ -44,12 +46,14 @@ public class Main extends javax.swing.JFrame {
         txtUser = new javaswingdev.raven.swing.TextField();
         alertLabel = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setLocation(new java.awt.Point(440, 205));
-        setMinimumSize(new java.awt.Dimension(950, 616));
+        setMinimumSize(new java.awt.Dimension(1104, 705));
         setUndecorated(true);
-        setPreferredSize(new java.awt.Dimension(987, 621));
+        setPreferredSize(new java.awt.Dimension(1104, 705));
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         bg.setBackground(new java.awt.Color(245, 245, 245));
@@ -58,6 +62,7 @@ public class Main extends javax.swing.JFrame {
 
         roundPanel1.setBackground(new java.awt.Color(255, 255, 255));
         roundPanel1.setRound(10);
+        roundPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         button1.setBackground(new java.awt.Color(98, 98, 157));
         button1.setForeground(new java.awt.Color(255, 255, 255));
@@ -67,14 +72,18 @@ public class Main extends javax.swing.JFrame {
                 button1ActionPerformed(evt);
             }
         });
+        roundPanel1.add(button1, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 270, 198, 40));
 
         jLabel1.setFont(new java.awt.Font("sansserif", 1, 24)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(106, 106, 164));
         jLabel1.setText("LOGIN");
+        roundPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 30, 177, 53));
+        roundPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 350, 343, -1));
 
-        button2.setBackground(new java.awt.Color(173, 108, 108));
+        button2.setBackground(new java.awt.Color(196, 72, 72));
         button2.setForeground(new java.awt.Color(255, 255, 255));
         button2.setText("CREATE NEW ACCOUNT");
+        roundPanel1.add(button2, new org.netbeans.lib.awtextra.AbsoluteConstraints(70, 370, 257, 41));
 
         jLabel2.setForeground(new java.awt.Color(153, 153, 153));
         jLabel2.setText("Forgot password?");
@@ -95,6 +104,7 @@ public class Main extends javax.swing.JFrame {
                 jLabel2MouseReleased(evt);
             }
         });
+        roundPanel1.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 320, 104, 25));
 
         txtPass.setLabelText("Password");
         txtPass.setLineColor(new java.awt.Color(102, 102, 102));
@@ -107,6 +117,7 @@ public class Main extends javax.swing.JFrame {
                 txtPassFocusLost(evt);
             }
         });
+        roundPanel1.add(txtPass, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 180, 302, 78));
 
         txtUser.setLabelText("Username");
         txtUser.setLineColor(new java.awt.Color(102, 102, 102));
@@ -123,77 +134,50 @@ public class Main extends javax.swing.JFrame {
                 txtUserActionPerformed(evt);
             }
         });
+        roundPanel1.add(txtUser, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 100, 302, 74));
 
         alertLabel.setFont(new java.awt.Font("SansSerif", 2, 14)); // NOI18N
         alertLabel.setForeground(new java.awt.Color(255, 0, 0));
-        alertLabel.setText("Incorrect username or password!");
+        alertLabel.setPreferredSize(new java.awt.Dimension(208, 13));
+        roundPanel1.add(alertLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 80, 218, -1));
 
-        javax.swing.GroupLayout roundPanel1Layout = new javax.swing.GroupLayout(roundPanel1);
-        roundPanel1.setLayout(roundPanel1Layout);
-        roundPanel1Layout.setHorizontalGroup(
-            roundPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(roundPanel1Layout.createSequentialGroup()
-                .addGroup(roundPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(roundPanel1Layout.createSequentialGroup()
-                        .addGap(107, 107, 107)
-                        .addComponent(button1, javax.swing.GroupLayout.PREFERRED_SIZE, 198, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(roundPanel1Layout.createSequentialGroup()
-                        .addGap(151, 151, 151)
-                        .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 104, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(roundPanel1Layout.createSequentialGroup()
-                        .addGap(77, 77, 77)
-                        .addComponent(button2, javax.swing.GroupLayout.PREFERRED_SIZE, 257, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(roundPanel1Layout.createSequentialGroup()
-                        .addGap(36, 36, 36)
-                        .addGroup(roundPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(txtPass, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(txtUser, javax.swing.GroupLayout.DEFAULT_SIZE, 343, Short.MAX_VALUE)
-                            .addComponent(jSeparator1)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(alertLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 218, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addContainerGap(34, Short.MAX_VALUE))
-        );
-        roundPanel1Layout.setVerticalGroup(
-            roundPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, roundPanel1Layout.createSequentialGroup()
-                .addGap(51, 51, 51)
-                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(alertLabel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtUser, javax.swing.GroupLayout.PREFERRED_SIZE, 74, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtPass, javax.swing.GroupLayout.PREFERRED_SIZE, 78, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(button1, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(18, 18, 18)
-                .addComponent(button2, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(133, 133, 133))
-        );
-
-        bg.add(roundPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(460, 60, -1, 460));
+        bg.add(roundPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(530, 140, 380, 450));
 
         jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(99, 99, 159));
         jLabel4.setText("GROUP OOP");
-        bg.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 30, 260, 140));
+        bg.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 50, 260, 140));
 
-        getContentPane().add(bg, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1330, 620));
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/Manage-Store.png"))); // NOI18N
+        jLabel5.setText("jLabel3");
+        bg.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(-220, 200, -1, -1));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/Manage-Store.png"))); // NOI18N
+        jLabel3.setText("jLabel3");
+        bg.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(840, 170, 570, -1));
+
+        getContentPane().add(bg, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 1110, 710));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void button1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button1ActionPerformed
         // TODO add your handling code here:
+          Object source = evt.getSource();
         if(txtUser.getText().equals("bms") && txtPass.getText().equals("123456")){
             this.setVisible(false);
             new Dashboard().setVisible(true);
-        }else{
-            alertLabel.setVisible(true);
+        }else if(txtUser.getText().equals("") || txtPass.getText().equals("")){
+           
+         alertLabel.setVisible(true);
+               alertLabel.setText("Incorrect username or password!");
+          
+        }  
+        else
+        {
+               alertLabel.setVisible(true);
+               alertLabel.setText("Incorrect username or password!");
+            
         }
     }//GEN-LAST:event_button1ActionPerformed
 
@@ -284,7 +268,9 @@ public class Main extends javax.swing.JFrame {
     private javaswingdev.swing.Button button2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JSeparator jSeparator1;
     private javaswingdev.swing.RoundPanel roundPanel1;
     private javaswingdev.swing.titlebar.TitleBar titleBar;
