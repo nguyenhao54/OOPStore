4db8bbdf3a8d04e85d5026619686f30cb7991b9a
ducanh11092001@gmail.com <ducanh11092001@gmail.com>
2022-07-12 14:13:41 +0700

    add bill active, fix product quantity bug

--- src/javaswingdev/main/Billing.java
+++ src/javaswingdev/main/Billing.java
@@ -5,20 +5,21 @@
 package javaswingdev.main;
 import java.awt.Color;
 import model.Bill;
-import java.util.Date;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import javaswingdev.form.Message;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
+import javaswingdev.swing.table.BillEventAction;
 import model.Product;
 import org.jdesktop.animation.timing.Animator;
 import org.jdesktop.animation.timing.TimingTarget;
 import org.jdesktop.animation.timing.TimingTargetAdapter;
 import javaswingdev.swing.table.OrderEventAction;
 import model.Order;
+import model.Staff;
 
 public class Billing extends javax.swing.JFrame {
     
@@ -29,8 +30,10 @@ public class Billing extends javax.swing.JFrame {
     private boolean ok;
     private boolean show = true;
     private Bill bill;
+    private ArrayList<Order> orderList = new ArrayList<>();
     private DefaultTableModel allBillModel;
     private javaswingdev.swing.table.Table allBillTable;
+    private BillEventAction billEventAction;
 
     public Bill getBill() {
         return bill;
@@ -38,6 +41,29 @@ public class Billing extends javax.swing.JFrame {
     
     public void setBill(Bill bill) {
         this.bill = bill;
+    }
+    
+    
+    /**
+     * @return the allBillTable
+     */
+    public javaswingdev.swing.table.Table getAllBillTable() {
+        return allBillTable;
+    }
+
+    /**
+     * @param allBillTable the allBillTable to set
+     */
+    public void setAllBillTable(javaswingdev.swing.table.Table allBillTable) {
+        this.allBillTable = allBillTable;
+    }
+    
+    public BillEventAction getBillEventAction(){
+        return billEventAction;
+    }
+    
+    public void setBillEventAction(BillEventAction bea){
+        this.billEventAction = bea;
     }
     
     public boolean isOk() {
@@ -61,13 +87,15 @@ public class Billing extends javax.swing.JFrame {
         billPaid.setText(Double.toString(bill.getPaid()));
         billReturn.setText(Double.toString(bill.getPaid() - bill.getTotalCost()));
         allBillModel = model;
-        this.allBillTable = allBillTable;
+        this.setAllBillTable(allBillTable);
         setVisible(true);
     }
     
     public Billing(java.awt.Frame parent, boolean modal) {
         initComponents();
         table1.fixTable(jScrollPane2);
+//       Avoid focus to staff name text field
+        jLabel3.requestFocusInWindow();
         initTable();
         centerComponent();
          getContentPane().setBackground(Color.WHITE);
@@ -101,7 +129,8 @@ public class Billing extends javax.swing.JFrame {
             public void delete(Order order) {
              if(msg.showMessage("Delete this order?")){
                  Product orderProduct = order.getProduct();
-                 orderProduct.setQuantity(orderProduct.getProductId() + 1);
+//               Give the order quantity back to product when delete order
+                 orderProduct.setQuantity(orderProduct.getQuantity() + order.getQuantity());
                 bill.deleteOrder(order.getOrderId());
                 billTotal.setText(Double.toString(bill.getTotalCost()));
                 model =(DefaultTableModel) table1.getModel();
@@ -145,13 +174,12 @@ public class Billing extends javax.swing.JFrame {
         jLabel3 = new javax.swing.JLabel();
         billDate = new javax.swing.JLabel();
         staffNameLabel = new javax.swing.JLabel();
-        staffName = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
         table1 = new javaswingdev.swing.table.Table();
-        quantity = new javaswingdev.raven.swing.TextField();
         jLabel4 = new javax.swing.JLabel();
         findProduct = new javaswingdev.swing.Button();
         productId = new javaswingdev.raven.swing.TextField();
+        cancelBtn = new javaswingdev.swing.Button();
         billReturn = new javaswingdev.raven.swing.TextField();
         price = new javaswingdev.raven.swing.TextField();
         billTotal = new javaswingdev.raven.swing.TextField();
@@ -159,7 +187,8 @@ public class Billing extends javax.swing.JFrame {
         addToBillBtn = new javaswingdev.swing.Button();
         name = new javaswingdev.raven.swing.TextField();
         okBtn3 = new javaswingdev.swing.Button();
-        roundPanel1 = new javaswingdev.swing.RoundPanel();
+        quantity = new javaswingdev.raven.swing.TextField();
+        staffName = new javax.swing.JTextField();
 
         setBackground(new java.awt.Color(204, 204, 0));
         setUndecorated(true);
@@ -172,7 +201,7 @@ public class Billing extends javax.swing.JFrame {
 
         billDate.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
         billDate.setForeground(new java.awt.Color(74, 74, 74));
-        getContentPane().add(billDate, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 20, 220, 30));
+        getContentPane().add(billDate, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 10, 220, 30));
 
         staffNameLabel.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
         staffNameLabel.setForeground(new java.awt.Color(74, 74, 74));
@@ -180,10 +209,6 @@ public class Billing extends javax.swing.JFrame {
         staffNameLabel.setDoubleBuffered(true);
         staffNameLabel.setFocusTraversalPolicyProvider(true);
         getContentPane().add(staffNameLabel, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 50, -1, -1));
-
-        staffName.setFont(new java.awt.Font("Dialog", 0, 12)); // NOI18N
-        staffName.setForeground(new java.awt.Color(74, 74, 74));
-        getContentPane().add(staffName, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 50, 250, 30));
 
         table1.setBackground(new java.awt.Color(255, 0, 102));
         table1.setModel(new javax.swing.table.DefaultTableModel(
@@ -225,9 +250,6 @@ public class Billing extends javax.swing.JFrame {
 
         getContentPane().add(jScrollPane2, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 110, 520, 250));
 
-        quantity.setLabelText("Quantity");
-        getContentPane().add(quantity, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 290, 280, 60));
-
         jLabel4.setFont(new java.awt.Font("sansserif", 1, 24)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(106, 106, 164));
         jLabel4.setText("Bill Info");
@@ -249,6 +271,16 @@ public class Billing extends javax.swing.JFrame {
             }
         });
         getContentPane().add(productId, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 110, 280, -1));
+
+        cancelBtn.setBackground(new java.awt.Color(204, 75, 75));
+        cancelBtn.setForeground(new java.awt.Color(255, 255, 255));
+        cancelBtn.setText("CANCEL");
+        cancelBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cancelBtnActionPerformed(evt);
+            }
+        });
+        getContentPane().add(cancelBtn, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 480, 116, 40));
 
         billReturn.setEditable(false);
         billReturn.setLabelText("Return");
@@ -292,10 +324,11 @@ public class Billing extends javax.swing.JFrame {
                 okBtn3ActionPerformed(evt);
             }
         });
-        getContentPane().add(okBtn3, new org.netbeans.lib.awtextra.AbsoluteConstraints(380, 470, 110, 40));
+        getContentPane().add(okBtn3, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 480, 110, 40));
 
-        roundPanel1.setBackground(new java.awt.Color(255, 255, 255));
-        getContentPane().add(roundPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 860, 530));
+        quantity.setLabelText("Quantity");
+        getContentPane().add(quantity, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 290, 280, 60));
+        getContentPane().add(staffName, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 40, 210, 30));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -316,12 +349,28 @@ public class Billing extends javax.swing.JFrame {
                     if(quan > pd.getQuantity()){
                         msg.showDialog("Out of stock!", "red");
                     }else{
-                        int orderId = bill.maxOrderId++;
-                        order= new Order(orderId,pd,quan);
-                        bill.addOrder(order);
-                        billTotal.setText(Double.toString(bill.getTotalCost()));
-                        pd.setQuantity(pd.getQuantity() - 1);
-                        table1.addRow(order.toRowTable(orderEventAction));
+//                      Bill is not null => update
+                        if(bill != null){
+                            int orderId = bill.maxOrderId++;
+                            order= new Order(orderId,pd,quan);
+                            bill.addOrder(order);
+                            billTotal.setText(Double.toString(bill.getTotalCost()));
+                            pd.setQuantity(pd.getQuantity() - quan);
+                            table1.addRow(order.toRowTable(orderEventAction));
+                        }else{
+//                          Bill is null -> new bill, add new order to temporary order list
+                            int orderId = orderList.size() + 1;
+                            order = new Order(orderId, pd, quan);
+                            orderList.add(order);
+                            double sum = 0;
+                            for(Order o: orderList){
+                                sum += o.getCost();
+                            }
+                            billTotal.setText(Double.toString(sum));
+                            pd.setQuantity(pd.getQuantity() - quan);
+                            table1.addRow(order.toRowTable(orderEventAction));
+                        }
+                        
                     }
              }else{
                  msg.showDialog("Product not found.", "red");
@@ -347,15 +396,89 @@ public class Billing extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_findProductActionPerformed
 
+    boolean checkNumeric(String strNum){
+        try {
+            double d = Integer.parseInt(strNum);
+            return true;
+        } catch (NumberFormatException nfe) {
+            return false;
+        }
+    }
+    
     private void okBtn3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_okBtn3ActionPerformed
         // TODO add your handling code here:
-        allBillModel.setValueAt(bill.getTotalCost(),allBillTable.getSelectedRow() , 3);
-        setVisible(false);
+//        Update part
+        Message msg = new Message();
+        if(bill != null){
+            if(Double.parseDouble(billPaid.getText()) < bill.getTotalCost()){
+                msg.showDialog("Insufficient paid", "red");
+            }else if(staffName.getText().equals("") || billPaid.getText().equals("")){
+                msg.showDialog("Fill in staff id & paid", "red");
+            }else{
+//              If not a number -> not update staff
+                if(!checkNumeric(staffName.getText())){
+                    allBillModel.setValueAt(bill.getTotalCost(),getAllBillTable().getSelectedRow() , 3);
+                    bill.setPaid(Double.parseDouble(billPaid.getText()));
+                    setVisible(false);
+                }else{
+                    Staff newStaff = Dashboard.store.findStaff(Integer.parseInt(staffName.getText()));
+                    if(newStaff == null){
+                        msg.showDialog("Staff not found!", "red");
+                    }else{
+//                      update staff because input is a valid id
+                        bill.setStaff(newStaff);
+                        bill.setPaid(Double.parseDouble(billPaid.getText()));
+//                      Change info on all bill table
+                        allBillModel.setValueAt(bill.getTotalCost(),getAllBillTable().getSelectedRow() , 3);
+                        allBillModel.setValueAt(bill.getStaff().getName(),getAllBillTable().getSelectedRow() , 2);
+                        setVisible(false);
+                    }
+                }
+                
+            }
+        }else{
+//           Create new bill
+            
+            if(staffName.getText().equals("") || billPaid.getText().equals("")){
+                msg.showDialog("Fill in staff id & paid", "red");
+            }else{
+                    int id = Dashboard.store.maxBillId++;
+                int staffId = Integer.parseInt(staffName.getText());
+                Staff staff = Dashboard.store.findStaff(staffId);
+                double paid = Double.parseDouble(billPaid.getText());
+                if(staff == null){
+
+                    msg.showDialog("Staff not found!", "red");
+                }else{
+                    LocalDateTime now = LocalDateTime.now();
+                    LocalDateTime formattedNow = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), 
+                            now.getHour(), now.getMinute(), now.getSecond());
+                    Bill b = new Bill(id, staff, orderList, formattedNow, paid);
+                    if(Double.parseDouble(billPaid.getText()) < b.getTotalCost()){
+                        msg.showDialog("Insufficient paid", "red");
+                    }else{
+                        Dashboard.store.addBill(b);
+                        setBill(b);
+                        getAllBillTable().addRow(b.toRowTable(billEventAction));
+                        setVisible(false);
+                    }
+                }
+            }
+        }
+        
     }//GEN-LAST:event_okBtn3ActionPerformed
 
     private void nameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_nameActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_nameActionPerformed
+
+    private void cancelBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cancelBtnActionPerformed
+        ok=false;
+        for(Order o: orderList){
+            o.getProduct().addQuantity(o.getQuantity());
+        }
+        closeMenu();
+    }//GEN-LAST:event_cancelBtnActionPerformed
 
     /**
      * @param args the command line arguments
@@ -367,6 +490,7 @@ public class Billing extends javax.swing.JFrame {
     private javaswingdev.raven.swing.TextField billPaid;
     private javaswingdev.raven.swing.TextField billReturn;
     private javaswingdev.raven.swing.TextField billTotal;
+    private javaswingdev.swing.Button cancelBtn;
     private javaswingdev.swing.Button findProduct;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -376,8 +500,7 @@ public class Billing extends javax.swing.JFrame {
     private javaswingdev.raven.swing.TextField price;
     private javaswingdev.raven.swing.TextField productId;
     private javaswingdev.raven.swing.TextField quantity;
-    private javaswingdev.swing.RoundPanel roundPanel1;
-    private javax.swing.JLabel staffName;
+    private javax.swing.JTextField staffName;
     private javax.swing.JLabel staffNameLabel;
     private javaswingdev.swing.table.Table table1;
     // End of variables declaration//GEN-END:variables
